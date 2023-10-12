d79fd297c9377380f5521d68d3d48a8b7730f5e1
BrandonRv <138633220+BrandonRv@users.noreply.github.com>
2023-10-12 13:36:13 -0500

    Commit-search-Brandon

--- src/components/Buscador.jsx
+++ src/components/Buscador.jsx
@@ -1,27 +1,50 @@
 import React, { useState } from 'react';
 import './Buscador.css';
 
-
 const Buscador = () => {
-    const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
+  const [results, setResults] = useState([]);
 
-    const handleSearchChange = (event) => {
-        setSearchTerm(event.target.value);
-        console.log(searchTerm)
-    }
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    fetch(`https://restcountries.com/v3.1/name/${searchTerm}`)
+      .then(response => response.json())
+      .then(data => {
+        setResults(data);
+      })
+      .catch(error => {
+        console.error('Error fetching data: ', error);
+      });
+  };
 
   return (
     <div className='container'>
-    <nav bg="light" expand="lg">
-        <form>
-        <input
-          type="text"
-          placeholder="Buscar"
-          value={searchTerm}
-          onChange={handleSearchChange}/>
-          <button variant="outline-success">Buscar</button>
+      <nav bg="light" expand="lg">
+        <form onSubmit={handleSearchSubmit}>
+          <input
+            type="text"
+            placeholder="Buscar"
+            value={searchTerm}
+            onChange={handleSearchChange}
+          />
+          <button type="submit" variant="outline-success">
+            Buscar
+          </button>
         </form>
-    </nav>
+      </nav>
+      {/* Renderizar los resultados */}
+      <div className="results">
+        {results.map(country => (
+          <div key={country.name.common}>
+            <h2>{country.name.common}</h2>
+            {/* Aqui es Para mostrar más detalles sobre los Paises */}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
